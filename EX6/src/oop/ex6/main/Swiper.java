0d12a6b0ddb1866970490ffe80bf590f03b80ced
yoav palti <yoav.palti@mail.huji.ac.il>
2018-06-21 10:40:55 +0300

    wow what a code you wrote

---
+++ EX6/src/oop/ex6/main/Swiper.java
@@ -0,0 +1,17 @@
+package oop.ex6.main;
+
+import oop.ex6.syntaxobject.CodeLine;
+
+import java.util.ArrayList;
+
+public class Swiper {
+
+    /**
+     * get a files line and split them acurding to { ; or }
+     * @param lines the sjava file lines
+     * @return the lines splited accordingly;
+     */
+    public static ArrayList<String> SplitLines(ArrayList<String> lines){
+
+    }
+}
