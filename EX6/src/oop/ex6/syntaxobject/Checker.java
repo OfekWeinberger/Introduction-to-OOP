9f7874866fcab39b7ddbd0c877a909a09fadcc85
Ofek Weinberger <ofekwa@gmail.com>
2018-06-27 23:14:56 +0300

    update

--- EX6/src/oop/ex6/syntaxobject/Checker.java
+++ EX6/src/oop/ex6/syntaxobject/Checker.java
@@ -4,5 +4,11 @@ import oop.ex6.syntaxobject.IllegalSyntaxException;
 import oop.ex6.syntaxobject.scope.Scope;
 
 public interface Checker {
+	/**
+	 * This method checks the given string if it is applicable to the s-Java specifications.
+	 * @param string The string we want to check.
+	 * @param scope The scope where this string lies.
+	 * @throws IllegalSyntaxException The exception thrown if the method finds a syntax error in the code.
+	 */
 	void check(String string, Scope scope) throws IllegalSyntaxException;
 }
