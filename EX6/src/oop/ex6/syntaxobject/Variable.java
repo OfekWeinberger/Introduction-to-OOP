725d5047af2b33ffc5dac67c37f652b4ab3700e7
Ofek Weinberger <ofekwa@gmail.com>
2018-06-27 22:34:35 +0300

    update

--- EX6/src/oop/ex6/syntaxobject/Variable.java
+++ EX6/src/oop/ex6/syntaxobject/Variable.java
@@ -2,12 +2,36 @@ package oop.ex6.syntaxobject;
 
 public class Variable {
 
-	private static final String CONST_EXCEPTION = "final variabel mast be initialized at decleration";
+
+	private static final String CONST_EXCEPTION = "final variable must be initialized at declaration";
+
+	/**
+	 * flag that holds if the variable is final.
+	 */
 	private final boolean FINAL;
+	/**
+	 * The type of the variable.
+	 */
 	private Type type;
+	/**
+	 * The name of the variable.
+	 */
 	private String name;
+	/**
+	 * flag that holds if the variable is assigned.
+	 */
 	private boolean assigned;
 
+
+	/**
+	 * The constructor for the variable.
+	 * @param type The type of the variable.
+	 * @param name The name of the variable.
+	 * @param assigned flag that holds if the variable is assigned.
+	 * @param FINAL flag that holds if the variable is final.
+	 * @throws IllegalSyntaxException if there is a final variable that is not assigned at declaration,
+	 * throw an IllegalSyntaxException, because that is illegal in the s-Java specifications.
+	 */
 	public Variable(Type type, String name, boolean assigned, boolean FINAL) throws IllegalSyntaxException {
 		this.type = type;
 		this.name = name;
@@ -18,22 +42,41 @@ public class Variable {
 		}
 	}
 
+	/**
+	 * Getter for the type.
+	 * @return type of the variable.
+	 */
 	public Type getType() {
 		return type;
 	}
 
+	/**
+	 * Getter for the name.
+	 * @return name of the variable.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Getter for the FINAL.
+	 * @return if variable is final.
+	 */
 	public boolean isFinal() {
 		return FINAL;
 	}
 
+	/**
+	 * Getter for the assigned.
+	 * @return if variable is assigned.
+	 */
 	public boolean isAssigned() {
 		return assigned;
 	}
 
+	/**
+	 * Setter for the assigned.
+	 */
 	public void setAssigned() {
 		this.assigned = true;
 	}
